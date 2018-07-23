467456d60a8224618b04034eb83ca52dc8e36f58
swetamish <sweta@ics-global.in>
2018-07-23 11:21:09 +0530

    commit and push

--- first_spring_boot_program/src/main/java/com/edureka/first_spring_boot_program/ApplicationConfiguration.java
+++ first_spring_boot_program/src/main/java/com/edureka/first_spring_boot_program/ApplicationConfiguration.java
@@ -1,12 +1,13 @@
 package com.edureka.first_spring_boot_program;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApplicationConfiguration {
 
 	@RequestMapping("/hello")
-	public String hello() {
-		return "Hello World!";
+	public String hello(@RequestParam(name="name",required=false,defaultValue="World") String name) {
+		return "Hello - "+name;
 	}
 }
