3b0734b0c0e9f5c91ff2aa4b264e261196af7d80
swetamish <sweta@ics-global.in>
2018-07-23 10:33:52 +0530

    -Hello_World_Demo

---
+++ first_spring_boot_program/src/main/java/com/edureka/first_spring_boot_program/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package com.edureka.first_spring_boot_program;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationConfiguration {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+}
---
+++ first_spring_boot_program/src/main/java/com/edureka/first_spring_boot_program/FirstSpringBootProgramApplication.java
@@ -0,0 +1,12 @@
+package com.edureka.first_spring_boot_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootProgramApplication.class, args);
+	}
+}
